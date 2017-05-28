9577d1e84a2aadec6a01a7dc74059cc46cb1a25b
Wes Basinger <wbasinger@villagetechschools.org>
2017-05-28 15:15:00 -0500

    Scaffolding out the view branching logic.

--- src/App.js
+++ src/App.js
@@ -2,18 +2,32 @@ import React, { Component } from 'react';
 
 import Header from './Components/Header';
 import Footer from './Components/Footer';
+import Feed from './Components/Feed';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      view: 'feed'
+    }
+  }
+
   render() {
+
+    var main = null;
+
+    if(this.state.view == 'feed')  {
+      main = <Feed />
+    }
+
     return (
       <div>
         <Header />
-        <div>
-          Main
-        </div>
+        { main }
         <Footer />
       </div>
-    );
+    )
   }
 }
 
---
+++ src/Components/Feed.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Feed extends Component {
+  render() {
+    return (
+      <div>
+        This section is the main feed.
+      </div>
+    );
+  }
+}
+
+export default Feed;
