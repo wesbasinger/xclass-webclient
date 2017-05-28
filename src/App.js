ef37447e63687c29678eec11dbe18389c0fe407e
Wes Basinger <wbasinger@villagetechschools.org>
2017-05-28 14:59:14 -0500

    Stubbed out header, main, and footer.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import Header from './Components/Header';
+import Footer from './Components/Footer';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div>
+        <Header />
+        <div>
+          Main
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Footer />
       </div>
     );
   }
---
+++ src/Components/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <button>Feed</button>
+        <button>View Classes</button>
+        <button>Register</button>
+        <button>My Enrollments</button>
+      </div>
+    );
+  }
+}
+
+export default Header;
