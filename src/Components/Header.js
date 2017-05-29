bda478f941409317e50616688c75e337b87b8ca5
Wes Basinger <wbasinger@villagetechschools.org>
2017-05-29 07:15:31 -0500

    Personalized the header bar.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -10,6 +10,9 @@ class Header extends Component {
           <button value="list" onClick={this.props.onNavClick}>List Classes</button>
           <button value="register" onClick={this.props.onNavClick}>Register</button>
           <button value="enrollments" onClick={this.props.onNavClick}>My Enrollments</button>
+          <span>
+            <img src={this.props.user.imageUrl} height="40px" alt=""/>Logged in as: {this.props.user.name} 
+          </span>
           <button onClick={this.props.onLogout}>Logout</button>
         </div>
       );
