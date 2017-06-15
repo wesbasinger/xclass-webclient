30b46ca35ae7dc700de133f3a0ce63594db2586a
Wes Basinger <wbasinger@villagetechschools.org>
2017-06-14 21:45:14 -0500

    Working towards being able to remove a student.

--- src/Components/Manage.js
+++ src/Components/Manage.js
@@ -5,42 +5,84 @@ class Manage extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {pick: ""}
+    this.state = {
+      pick: "",
+      managing: null
+    }
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleOptionChange = this.handleOptionChange.bind(this);
+    this.handleOptionSubmit = this.handleOptionSubmit.bind(this);
   }
 
-  handleChange(e) {
+  handleOptionChange(e) {
     this.setState({pick: e.target.value})
   }
 
-  handleSubmit(e) {
+  handleOptionSubmit(e) {
     e.preventDefault();
-    this.props.onRegistrationSubmit({pick: this.state.pick});
+
+    var self = this;
+
+    this.props.classes.forEach(function(_class) {
+      if(_class._id === self.state.pick) {
+        self.setState({managing: _class})
+      }
+    })
   }
 
   render() {
 
-    return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <select value={this.state.pickk} onChange={this.handleChange}>
-            <option></option>
-            {
-              this.props.classes.map(function(open_class) {
-                return(
-                  <option key={open_class._id} value={open_class._id}>
-                    {open_class.title}
-                  </option>
-                )
-              })
-            }
-          </select>
-          <input type="submit" value="Submit" />
-        </form>
-      </div>
-    );
+    if(!this.state.managing) {
+      return (
+        <div>
+          <form onSubmit={this.handleOptionSubmit}>
+            <select value={this.state.pickk} onChange={this.handleOptionChange}>
+              <option></option>
+              {
+                this.props.classes.map(function(open_class) {
+                  return(
+                    <option key={open_class._id} value={open_class._id}>
+                      {open_class.title}
+                    </option>
+                  )
+                })
+              }
+            </select>
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <h1>{this.state.managing.title}</h1>
+          <table>
+            <thead>
+              <tr>
+                <td>Image</td>
+                <td>Name</td>
+                <td>Email</td>
+                <td>Action</td>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                this.state.managing.students.map(function(student) {
+                  return(
+                    <tr>
+                      <td><img src={student.imageUrl}/></td>
+                      <td>{student.name}</td>
+                      <td>{student.email}</td>
+                      <td><button>Remove</button></td>
+                    </tr>
+                  )
+                })
+              }
+            </tbody>
+          </table>
+        </div>
+      )
+    }
   }
 }
 
